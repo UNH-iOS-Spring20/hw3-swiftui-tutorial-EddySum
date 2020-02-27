8753cffb5cc5dfbba5fb728486a242bf770872ee
Eddy Sumra <adnansumra115@gmail.com>
2020-02-27 15:11:49 -0500

    HW 3 Part 3

---
+++ StartingPoint/Landmarks/Landmarks/UserData.swift
@@ -0,0 +1,22 @@
+//
+//  UserData.swift
+//  Landmarks
+//
+//  Created by Adnan Sumra on 2/27/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+import combine
+
+struct UserData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserData_Previews: PreviewProvider {
+    static var previews: some View {
+        UserData()
+    }
+}
